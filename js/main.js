412d83972fe13aab35bc296ff013668e8101e120
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-11 04:39:36 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 require.config({
-  paths: {
-    "jsdelivr": "https://cdn.jsdelivr.net/gh/memoriagame/js/memoriagame/main",
-    "githubPages": "https://memoriagame.github.io/js/memoriagame/main",
-    "githubusercontent": "https://raw.githubusercontent.com/memoriagame/js/main/memoriagame/main"
-  }
-});
+ paths: {
+   "jsdelivr": "https://cdn.jsdelivr.net/gh/memoriagame/js/memoriagame/main",
+   "githubPages": "https://memoriagame.github.io/js/memoriagame/main",
+   "githubusercontent": "https://raw.githubusercontent.com/memoriagame/js/main/memoriagame/main"
+  }});
 
-try {
- require(['jsdelivr'], function() {
-   console.log('jsdelivr is loaded.')
+try{
+  require(['jsdelivr'], function(){
+    console.log('jsdelivr is loaded.')
   });
-} catch (error) {
-   try {
-    require(['githubPages'], function() {
-   console.log('githubPages is loaded.')
-  });
-   } catch (error) {
-    try {
-   require(['githubusercontent'], function() {
-   console.log('githubusercontent is loaded.')
-  });
-} catch (error) {
-  console.error(error);
-  module = require('./js/main');
-  // Expected output: ReferenceError: nonExistentFunction is not defined
-  // (Note: the exact output may be browser-dependent)
 }
-}
-  // Expected output: ReferenceError: nonExistentFunction is not defined
-  // (Note: the exact output may be browser-dependent)
+catch (error){
+  try{
+    require(['githubPages'], function(){
+      console.log('githubPages is loaded.')
+    });
+  }
+  catch (error){
+    try{
+      require(['githubusercontent'], function(){
+        console.log('githubusercontent is loaded.')
+      });
+    }
+    catch (error){
+      // Expected output: ReferenceError: nonExistentFunction is not defined
+      // (Note: the exact output may be browser-dependent)
+      console.error(error);
+      module = require('./js/main');
+    }
+  }
 }
