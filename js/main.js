f2fd9b599334924700f0374c762498a203a879ea
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-11 04:36:53 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 require.config({
   paths: {
     "jsdelivr": "https://cdn.jsdelivr.net/gh/memoriagame/js/memoriagame/main",
-     "githubPages": "https://memoriagame.github.io/js/memoriagame/main",
+    "githubPages": "https://memoriagame.github.io/js/memoriagame/main",
     "githubusercontent": "https://raw.githubusercontent.com/memoriagame/js/main/memoriagame/main"
   }
 });
 
-
 try {
-  require(['jsdelivr'], function() {
+ require(['jsdelivr'], function() {
    console.log('jsdelivr is loaded.')
   });
-  } catch(error) {
-    // Do something as a fallback. For example: 
+} catch (error) {
+   try {
+    require(['githubPages'], function() {
+   console.log('githubPages is loaded.')
+  });
+   } catch (error) {
     try {
-      require(['githubPages'], function() {
-        console.log('githubPages is loaded.')
-        });
-        } catch(error) {
-          // Do something as a fallback. For example: 
-         try {
-      require(['githubusercontent'], function() {
-        console.log('githubusercontent is loaded.')
-        });
-        } catch(error) {
-            module = require('./js/main');
-         }
-        }
-      }
-    }    
-  }
+   require(['githubusercontent'], function() {
+   console.log('githubusercontent is loaded.')
+  });
+} catch (error) {
+  console.error(error);
+  module = require('./js/main');
+  // Expected output: ReferenceError: nonExistentFunction is not defined
+  // (Note: the exact output may be browser-dependent)
+}
+}
+  // Expected output: ReferenceError: nonExistentFunction is not defined
+  // (Note: the exact output may be browser-dependent)
 }
