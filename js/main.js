1f2ecc39933f4696b592f542dfaf80cf1cc250f4
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-11 04:07:33 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+require.config({
+  paths: {
+    "jsdelivr": "https://cdn.jsdelivr.net/gh/memoriagame/js/memoriagame/main.js",
+		"githubPages":	"https://memoriagame.github.io/js/memoriagame/main.js",
+    "githubusercontent": "https://raw.githubusercontent.com/memoriagame/js/main/memoriagame/main.js"
+  }
+});
+
+require(['jsdelivr'], function() {
+  console.log('jsdelivr is loaded.')
+});
+
+require(['githubPages'], function() {
+  console.log('githubPages is loaded.')
+});
+
+require(['githubusercontent'], function() {
+  console.log('githubusercontent is loaded.')
+});
